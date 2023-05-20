b08d94546658cb17be77810f4082202d84019c76
Utkarsh4517 <uttkarsh.dav@gmail.com>
2023-05-21 00:25:44 +0530

    created login data

---
+++ lib/screens/auth_screens/login_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool _loadingButton = false;
+
+  Map<String, String> data = {};
+
+  void switchLogin(){
+    setState(() {
+      
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/widgets/login_data.dart
@@ -0,0 +1,16 @@
+
+class LoginData {
+  static Map<String, String> signIn = {
+    "heading": "Getting Started",
+    "subHeading": "Create an account to continue",
+    "label": "SIGN_IN",
+    "footer": "Create a new account? Sign Up"
+  };
+
+    static Map<String, String> signUp = {
+    "heading": "Welcome back",
+    "subHeading": "Use your account email and password",
+    "label": "SIGN_UP",
+    "footer": "Already have an account? Sign In"
+  };
+}
