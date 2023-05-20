50bed1e377161ca8d2e02abe1318075fbbe52adb
Utkarsh4517 <uttkarsh.dav@gmail.com>
2023-05-20 12:07:06 +0530

    added colors constants

---
+++ lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color orange = Color(0x00ff8811);
+const Color jasmine = Color(0x00f4d06f);
+const Color floralWhite = Color(0x00fff8f0);
+const Color tiffanyBlue = Color(0x009dd9d2);
+const Color spaceCadet = Color(0x00392f5a);
