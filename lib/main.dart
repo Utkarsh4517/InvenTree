2b4e544eec735cd65dd45391ce94d5659a4db06f
Utkarsh4517 <uttkarsh.dav@gmail.com>
2023-05-20 16:45:25 +0530

    added firebase project

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:inventree/firebase_options.dart';
 import 'package:inventree/screens/homepage.dart';
 import 'package:inventree/widgets/navbar.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
   runApp(const MyApp());
 }
 
